1b18fa234971a19ff8064b0d7629a38a7c905531
Federico Iza <fedegiza@gmail.com>
2025-04-01 16:01:57 -0300

    Fix encoding issues.

--- src/raw_data.py
+++ src/raw_data.py
@@ -3,7 +3,7 @@ import wikipedia as wp
 
 
 def get_raw_data():
-    html = wp.page("The_Economist_Democracy_Index").html().encode("UTF-8")
+    html = wp.page("The_Economist_Democracy_Index").html().encode("utf-8")
 
     try:
         df = pd.read_html(html)[5]
@@ -11,6 +11,12 @@ def get_raw_data():
         raise ValueError("The expected table was not found on the Wikipedia.")
 
     df.rename(columns={"Regime type": "RegimeType"}, inplace=True)
+    df = df.map(
+        lambda x: x.replace("Asia and Austral\xadasia",
+                            "Asia and Australasia").replace(
+                                "Latin America and the Carib\xadbean",
+                                "Latin America and the Caribbean"
+                            ) if isinstance(x, str) else x)
     df.to_csv("data/raw/democracy_index.csv", index=False)
 
 
